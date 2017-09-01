187996a032a84a5d78b56488ff9304fd8672a24a
Eric Luchetta <eric@localhost.localdomain>
2017-09-01 13:24:42 +0200

    nouveau fichier : scripts/vol_down.sh 	nouveau fichier : scripts/vol_up.sh

---
+++ scripts/vol_down.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+vol=$({ echo "?V"; sleep 0.1; } | telnet Ampli_salon | grep -o -E '[0-9]+') 
+vol=${vol:4}
+vol=$(printf "%.3d" $(expr $vol - 20))
+cmd=$vol"VL"
+sleep 0.2
+{ echo "$cmd"; sleep 0.1; } | telnet Ampli_salon
