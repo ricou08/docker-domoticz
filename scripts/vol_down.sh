27c6671f5b6cfda3205307abef0d1c59b5eeff02
Eric Luchetta <eric@localhost.localdomain>
2017-10-10 21:28:16 +0200

    modifié :         scripts/vol_down.sh supprimé :        Dockerfile.debian

--- scripts/vol_down.sh
+++ scripts/vol_down.sh
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 #!/bin/bash
 
-vol=$({ echo "?V"; sleep 0.1; } | telnet Ampli_salon | grep -o -E '[0-9]+')
-=======
-#!/bin/sh
-
 vol=$({ echo "?V"; sleep 0.1; } | telnet Ampli_salon | grep -o -E '[0-9]+') 
-vol=${vol:4}
->>>>>>> 187996a032a84a5d78b56488ff9304fd8672a24a
 vol=$(printf "%.3d" $(expr $vol - 20))
 cmd=$vol"VL"
 sleep 0.2
